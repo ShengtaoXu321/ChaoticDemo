713f5ab770f654958e8e79a4ea44f67a10ef3601
徐胜涛 <3070448639@qq.com>
2019-09-11 16:23:07 +0800

    修改

--- 图2.15/Bao1_LE.m
+++ 图2.15/Bao1_LE.m
@@ -4,7 +4,7 @@
 %%%Time:2019-8-26                                 
 %%%Discript:子函数                                      
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function dX=HyBao1_LE(t,X)
+function dX=Bao1_LE(t,X)
 global a b c
 x=X(1); y=X(2); z=X(3);
 Y= [X(4), X(7), X(10);
@@ -12,11 +12,11 @@ Y= [X(4), X(7), X(10);
     X(6), X(9), X(12)];
 dX=zeros(9,1);
 %% 混沌系统方程
-dX(1)=a*(x-y);
-dX(2)=x*z-c*y;
-dX(3)=x*x-b*z;
+dX(1) = a*(x-y);
+dX(2) = x*z-c*y;
+dX(3) = x*x-b*z;
 %% Jacobi矩阵
 Jaco= [ a  -a  0;
         z  -c  x;
        2*x  0  -b];
-dX(4:12)=Jaco*Y
+dX(4:12) = Jaco*Y;
--- 图2.15/main.m
+++ 图2.15/main.m
@@ -11,7 +11,7 @@ a=20; c=32;
 for i=1:1:501
     b=0.03*(i-1);
     [T,Res]=lyapunov(3,@Bao1_LE,@ode45,0,0.001,500,[10 10 10],0);
-plot(T,Res);  %%画随时间变化的Lyapunov指数谱
+%plot(T,Res);  %%画随时间变化的Lyapunov指数谱
     Lyapunov1(i) = Res(end,1);
     Lyapunov2(i) = Res(end,1);
     Lyapunov3(i) = Res(end,1);
